3ea34cc4d119bdb20b17f2c673616732e0c5f1d2
huanle0610 <huanle0610@qq.com>
2020-07-10 19:41:57 +0800

    ci: add build

---
+++ build/precache-manifest.41adf61e9e895b45a5ea98dc4c1406be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b21c2dace0cd5da69ced097e2a425d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "31f37c34d3c8be162bfd",
+    "url": "./static/css/2.1b0424f1.chunk.css"
+  },
+  {
+    "revision": "31f37c34d3c8be162bfd",
+    "url": "./static/js/2.38f7f6fd.chunk.js"
+  },
+  {
+    "revision": "8ba85a39f7fa02007dc5",
+    "url": "./static/js/main.9c8c6e08.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
